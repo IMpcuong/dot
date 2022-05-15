2fda42a58a85afd0442e2d22dc713daec2998614
CuongTrinh1280 <58588543+CuongTrinh1280@users.noreply.github.com>
2022-05-15 23:51:26 +0700

    Change directory name

--- pwsh_script/get_sizes.ps1
+++
@@ -1,6 +0,0 @@
-Get-ChildItem -Path . -ErrorAction silentlycontinue -Recurse |
-Sort-Object -Property length -Descending |
-Format-Table -autosize -wrap -property `
-@{ Label = "Last access"; Expression = { ($_.LastWriteTime).ToshortDateString() } },
-@{ Label = "Size in MB"; Expression = { "{0:N2}" -f ($_.Length / 1MB) } },
-fullname
