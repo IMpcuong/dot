9e8f5d10d3e0141cb7b150341f85c4ca8dad2d37
IMpossible <58588543+IMpcuong@users.noreply.github.com>
2022-06-13 08:33:57 +0700

    chore: Sample script to install `choco` pkg-mngr
    `pkg-mngr` stands for package manager tool.

---
+++ pwsh-script/choco_install.ps1
@@ -0,0 +1,10 @@
+# Release note Windows Terminal: https://github.com/microsoft/terminal/tags
+
+# Download choco
+Set-ExecutionPolicy Bypass -Scope Process -Force `
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072 `
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Powershell v7: Import-Module Appx -UseWindowsPowershell
+# Powershell v6:
+Add-AppxPackage -Path .\Microsoft.WindowsTerminal_1.10.2383.0_8wekyb3d8bbwe.msixbundle
