92f20e590c12ba957690ab6eaff8262d6f48e4d4
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2022-07-07 09:24:34 +0700

    chore: Add some more awk examples

---
+++ bash-script/utilities/awk_used_disk.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# `$3` := Column 3 = Total amount of used disk space.
+# `$0` := All the fields.
+df -h | awk '{ if($3 > 0) { print $0 } }'
+
+# `awk` examples:
+
+# Assigned variable:
+awk 'BEGIN { cnt = 2; cnt ^= 4; print "Counter =", cnt }'
+
+# Logical:
+awk 'BEGIN { name = ""; if(!length(name)) print "name is empty string." }'
+awk 'BEGIN { \
+  ch = "\n"; if (ch == " " || ch == "\t" || ch == "\n") \
+  print "Current character is whitespace." \
+}'
+
+# String concatenation:
+awk 'BEGIN { str1 = "Hello, "; str2 = "World"; str3 = str1 str2; print str3 }'
+
+# Ternary and unary:
+awk 'BEGIN { a = 10; b = 20; (a > b) ? max = a : max = b; print "Max =", max}'
+awk 'BEGIN { a = -10; a = -a; print "a =", a }'
+
+# Exponetial:
+awk 'BEGIN { a = 10; a = a**2; print "a =", a }'
+awk 'BEGIN { a = 10; a = a^2; print "a =", a }'
--- bash-script/utilities/used_disk.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# `$3` := Column 3 = Total amount of used disk space.
-# `$0` := All the fields.
-df -h | awk '{ if($3 > 0) { print $0 } }'
