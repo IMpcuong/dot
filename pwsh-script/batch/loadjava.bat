7cf224360a37c0651167a4eee548758d2919b73d
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2022-07-19 09:21:47 +0700

    minor: Fix variable declaration syntax errors

--- pwsh-script/batch/loadjava.bat
+++ pwsh-script/batch/loadjava.bat
@@ -1,16 +1,20 @@
 @echo off
-if "%OS%"=="Windows_NT" setlocal
+
+if "%OS%"=="Windows_NT" or "%OS%"=="" setlocal
+
 REM all variables defined local
-REM set DBG=echo to debug this script.
+REM set DBG=echo to debug this script
+
 set DBG=REM
 set nextarg=
 set rslvarg=
 set javaprops=
 set args=
 set jreclasspath="E:\App\Java\jdk\jre\lib\rt.jar;E:\App\Java\jdbc\lib\ojdbc.jar"
+
 :LOOP
 %DBG% LOOP-%CNT% (loop for args parsing)
-  set/a CNT+=1
+  set /a CNT+=1
   set nextarg=%1
   set nextarg=%nextarg:"=%
   if {%1} == {} goto MARK6
@@ -28,6 +32,7 @@ set jreclasspath="E:\App\Java\jdk\jre\lib\rt.jar;E:\App\Java\jdbc\lib\ojdbc.jar"
     %DBG% %args%
     shift
     goto LOOP
+
 :MARK1
 %DBG% MARK1 hit (-classpath)
   set CLASSPATH=%2
@@ -35,6 +40,7 @@ set jreclasspath="E:\App\Java\jdk\jre\lib\rt.jar;E:\App\Java\jdbc\lib\ojdbc.jar"
   shift
   shift
   goto LOOP
+
 :MARK2
 %DBG% MARK2 hit (first -addclasspath)
   if not "%addclasspath%"=="" goto MARK3
@@ -43,6 +49,7 @@ set jreclasspath="E:\App\Java\jdk\jre\lib\rt.jar;E:\App\Java\jdbc\lib\ojdbc.jar"
     shift
     shift
     goto LOOP
+
 :MARK3
 %DBG% MARK3 hit (additional -addclasspath)
   set nextarg=%2
@@ -50,6 +57,7 @@ set jreclasspath="E:\App\Java\jdk\jre\lib\rt.jar;E:\App\Java\jdbc\lib\ojdbc.jar"
   shift
   shift
   goto LOOP
+
 :MARK4
 %DBG% MARK4 hit (-resolver)
   set RESOLVER=%1
@@ -59,6 +67,7 @@ set jreclasspath="E:\App\Java\jdk\jre\lib\rt.jar;E:\App\Java\jdbc\lib\ojdbc.jar"
   shift
   shift
   goto LOOP
+
 :MARK5
 %DBG% MARK5 hit (-Djavaprop=prop)
   set javaprops=%javaprops% %1=%2
@@ -66,6 +75,7 @@ set jreclasspath="E:\App\Java\jdk\jre\lib\rt.jar;E:\App\Java\jdbc\lib\ojdbc.jar"
   shift
   shift
   goto LOOP
+
 :MARK10
 %DBG% MARK10 hit (-P or -password)
   set LJPASS=%1
@@ -74,6 +84,7 @@ set jreclasspath="E:\App\Java\jdk\jre\lib\rt.jar;E:\App\Java\jdbc\lib\ojdbc.jar"
   shift
   shift
   goto LOOP
+
 :MARK11
 %DBG% MARK11 hit (-u)
   set LJUSER=%1 %2
@@ -81,6 +92,7 @@ set jreclasspath="E:\App\Java\jdk\jre\lib\rt.jar;E:\App\Java\jdbc\lib\ojdbc.jar"
   shift
   shift
   goto LOOP
+
 :MARK12
 %DBG% MARK12 hit (-debug)
 REM debug requires the use of the jdbc debug jar
@@ -88,16 +100,19 @@ REM debug requires the use of the jdbc debug jar
   set args=%args% %1
   shift
   goto LOOP
+
 :MARK6
 %DBG% MARK6 hit (append classpath)
   if "%CLASSPATH%"=="" goto MARK7
     set jreclasspath="%jreclasspath%;%CLASSPATH%"
     set jreclasspath="%jreclasspath:"=%"
+
 :MARK7
 %DBG% MARK7 hit (append addclasspath)
   if "%addclasspath%"=="" goto MARK8
     set jreclasspath="%jreclasspath%;%addclasspath%"
     set jreclasspath="%jreclasspath:"=%"
+
 :MARK8
 %DBG% MARK8 hit (invoke loadjava)
 %DBG% *** CLASSPATH = %CLASSPATH% ***
