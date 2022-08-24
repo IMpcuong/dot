a9b368231a41994ab9ac49e8393d3d20428e8079
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2022-08-24 11:07:32 +0700

    script: Fix variable declaration keyword syntax in Bash

--- bash-script/utilities/kill_multi_procs.sh
+++ bash-script/utilities/kill_multi_procs.sh
@@ -2,13 +2,13 @@
 
 # NOTE: pidof <image_name>/<app_name>
 
-local proc=$( ps aux | awk '{ print $2, $11 }' OFS='\t' | grep $1 )
+declare proc=$( ps aux | awk '{ print $2, $11 }' OFS='\t' | grep $1 )
 # NOTE:
 # + With `$1 = python`
 # + `echo $proc` -> stdout := "1142 python 27263 python 58476 python"
 # -> The PID has been only located in the odd index ~ 2n + 1, n >= 0
 
-local mixtureArr=($proc)
+declare mixtureArr=($proc)
 declare -i arrLen=${#mixtureArr[@]}
 
 # Solution1:
