cba37186acb4a0c69f804f8966dfa9e6e6836765
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2022-08-26 09:31:35 +0700

    script: Remove all exited images from Docker env

---
+++ bash-script/utilities/rm_exited_docker_img.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# NOTE:
+# + `docker ps -a` := shows a list of all images.
+# + `awk ...` := prints the IDs of all images having the status equivalence with "Exited".
+# + `tail -n +2` := omits/ignores the first line/record from the precedence command output.
+# + `xargs docker rm` := removes all of the "Exited" images with the corresponded IDs.
+docker ps -a | \
+  awk '{ if ($5 ~ /.*Exited.*/) print $1 }' | \
+  tail -n +2 | \
+  xargs docker rm
