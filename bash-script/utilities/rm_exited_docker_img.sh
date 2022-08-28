3310b626eff61cdb47dc8c92e48713285962541b
IMpossible <58588543+IMpcuong@users.noreply.github.com>
2022-08-28 20:55:20 +0700

    chore: Documented matching regex pattern in `awk`

--- bash-script/utilities/rm_exited_docker_img.sh
+++ bash-script/utilities/rm_exited_docker_img.sh
@@ -3,6 +3,7 @@
 # NOTE:
 # + `docker ps -a` := shows a list of all images.
 # + `awk ...` := prints the IDs of all images having the status equivalence with "Exited".
+#             := the string insides double backslashes are represented for a regex pattern in `awk`. 
 # + `tail -n +2` := omits/ignores the first line/record from the precedence command output.
 # + `xargs docker rm` := removes all of the "Exited" images with the corresponded IDs.
 docker ps -a | \
