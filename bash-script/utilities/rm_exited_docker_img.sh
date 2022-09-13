ed6f03a252e69d821718432ee7bf0b2dd3128916
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2022-09-13 14:43:21 +0700

    script: Docker remove unused image

--- bash-script/utilities/rm_exited_docker_img.sh
+++ bash-script/utilities/rm_exited_docker_img.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 # NOTE:
-# + `docker ps -a` := shows a list of all images.
-# + `awk ...` := prints the IDs of all images having the status equivalence with "Exited".
-#             := the string inside double backslashes are represented for a regex pattern in `awk`. 
+# + `docker ps -a` := shows a list of all containers.
+# + `awk ...` := prints the IDs of all containers having the status equivalence with "Exited".
+#             := the string inside double backslashes are represented for a regex pattern in `awk`.
 # + `tail -n +2` := omits/ignores the first line/record from the precedence command output.
 # + `xargs docker rm` := removes all of the "Exited" images with the corresponded IDs.
 docker ps -a | \
   awk '{ if ($5 ~ /.*Exited.*/) print $1 }' | \
   tail -n +2 | \
   xargs docker rm
+
+# NOTE: remove first matched `docker` image filtered by name.
+docker images -a | \
+  grep "image_name" | \
+  head -n1 | \
+  awk '{ print $3 }' | \
+  xargs docker rmi -f
