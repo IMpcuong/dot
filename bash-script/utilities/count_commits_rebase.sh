7a367aa94b55cc63eaf3342eb12435370f7776fc
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2022-09-19 18:14:05 +0700

    script: Implement another solution for search commit by its hash

--- bash-script/utilities/count_commits_rebase.sh
+++ bash-script/utilities/count_commits_rebase.sh
@@ -5,4 +5,7 @@ declare short_hash=$(git rev-parse --short ${full_hash})
 
 # `nl`: number pf all lines.
 declare pos_rebase=$(git rev-list HEAD | nl | grep ${short_hash} | awk '{ print $1 }')
+if (( ${pos_rebase} == 0 )); then
+  pos_rebase=$(git rev-list --abbrev-commit HEAD | nl | grep ${short_hash} | awk '{ print $1 }')
+fi
 git rebase -i HEAD~${pos_rebase}
