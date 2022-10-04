aaedfc2a9b6883ca23040ad1db08e51306b1ea25
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2022-10-04 14:28:34 +0700

    script: xargs check logs of all inheritance processes by PID

--- bash-script/utilities/xargs_examples.sh
+++ bash-script/utilities/xargs_examples.sh
@@ -4,5 +4,12 @@
 ls -la | \
   grep '^-' | \
   awk '{ print $9 }' | \
-  xargs -I {} sh -c 'head -1 {}; tail -10 {}' | \
+  xargs -I{} sh -c 'head -1 {}; tail -10 {}' | \
   less
+
+# Exp2: Using `journalctl` to check all logs of a process by using PID itself (Process ID):
+ps aux | \
+  grep sshd | \
+  awk '{ print $2 }' | \
+  tail -n+1 | \
+  xargs -I{} sh -c 'journalctl _PID={}'
