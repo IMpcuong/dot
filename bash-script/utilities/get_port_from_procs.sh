c0a8ee886d818983cc865fc30ddc3f82270b29dc
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2022-10-31 16:10:09 +0700

    script: Retrieves ports from multi-procs

---
+++ bash-script/utilities/get_port_from_procs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+declare -x svc=$1
+
+if [[ "$svc" == "" ]]; then svc="java"; fi
+# NOTE: the pattern `/^ES/` equivalent with the status of connection have been `ESTABLISHED`
+# with the remmote host successfully.
+#
+# + `$4` := local address.
+# + `$5` := foreign address.
+netstat -tulpan | \
+  grep -i $svc | \
+  awk '{ if ($6 ~ /^ES/) print $4 "\t" $5 }'
