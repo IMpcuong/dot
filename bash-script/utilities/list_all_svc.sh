78ba8c96cec387641fc1bf9bfa4c494e13a35edf
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2023-03-21 16:50:30 +0700

    script: systemctl retrieve /usr/lib/systemd/system/*.service config-file

--- bash-script/utilities/list_all_svc.sh
+++ bash-script/utilities/list_all_svc.sh
@@ -5,3 +5,25 @@ sudo systemctl list-units --type service -all
 sudo systemctl list-unit-files
 sudo systemctl status -l nginx.service
 sudo systemctl is-active nginx.service
+
+sudo systemctl cat nginx.service
+# Output:
+# [Unit]
+# Description=nginx - high performance web server
+# After=network-online.target remote-fs.target nss-lookup.target
+# Wants=network-online.target
+# ConditionPathExists=/etc/nginx/nginx.conf
+#
+# [Service]
+# Type=forking
+# PIDFile=/var/run/nginx.pid
+# ExecStartPre=/usr/sbin/nginx -t
+# ExecStart=/usr/sbin/nginx -c /etc/nginx/nginx.conf
+# ExecReload=/bin/sh -c "/bin/kill -s HUP $MAINPID"
+# ExecStop=/bin/sh -c "/bin/kill -s TERM $MAINPID"
+#
+# Open files
+# LimitNOFILE=100000
+#
+# [Install]
+# WantedBy=multi-user.target
