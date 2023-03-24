b9a03b336169963541fcc93d85c37629e04592e4
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2023-03-24 09:08:09 +0700

    chore(script): Bash variable's identifier completion

--- bash-script/utilities/fdisk_used.sh
+++ bash-script/utilities/fdisk_used.sh
@@ -2,10 +2,10 @@
 
 set +x
 
-used_disk=$(
-  df -h | \
-  awk '{ if ($3 > 0) print $0; }' | \
-  wc -l
+declare -x used_disk=$(
+  df -h |
+    awk '{ if ($3 > 0) print $0; }' |
+    wc -l
 )
 
 echo -e "Number of file system disks that have been used: $((used_disk - 1))"
