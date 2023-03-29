9c15268633bd4594249f44524022518a1a4e15d2
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2023-03-29 18:50:01 +0700

    script: Download a desired MySQL community server RPM package using `xargs`

--- bash-script/utilities/xargs_examples.sh
+++ bash-script/utilities/xargs_examples.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 # Exp1: Retrieves multiple sections/chunks from all files existed in the current directory:
-ls -la | \
-  grep '^-' | \
-  awk '{ print $9 }' | \
-  xargs -I{} sh -c 'head -1 {}; tail -10 {}' | \
+ls -la |
+  grep '^-' |
+  awk '{ print $9 }' |
+  xargs -I{} sh -c 'head -1 {}; tail -10 {}' |
   less
 
 # Exp2: Using `journalctl` to check all logs of a process by using PID itself (Process ID):
-ps aux | \
-  grep sshd | \
-  awk '{ print $2 }' | \
-  tail -n+1 | \
+ps aux |
+  grep sshd |
+  awk '{ print $2 }' |
+  tail -n+1 |
   xargs -I{} sh -c 'journalctl _PID={}'
+
+# Exp2: Download a desired MySQL community server RPM package from the MySQL Yum Repository official repositories.
+curl -s "https://dev.mysql.com/downloads/repo/yum/" |
+  grep -oE "mysql80.*-el7-.*rpm" |
+  head -n1 |
+  xargs -I{} sh -c 'curl -sSLO "https://dev.mysql.com/get/{}"'
