934c4255844998787e6b27b58cfb4e1d74851465
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2023-03-30 15:32:19 +0700

    chore: Beautify following shfmt-tool's convention; fixed grammar errors

--- bash-script/utilities/manage_dirname_with_spaces.sh
+++ bash-script/utilities/manage_dirname_with_spaces.sh
@@ -2,21 +2,20 @@
 
 # Resource: https://www.cyberciti.biz/tips/handling-filenames-with-spaces-in-bash.html
 
-# Exp1: Using `du` to measure all directories existed in our current location.
+# Exp1: Using `du` to measure all directories that were existing in our current location.
 find . -maxdepth 1 -type d ! -empty -regex ".*[^[:space:]*].*" | xargs du -h --max-depth=1
 
-# Exp2: Using `find -print0` to extricate/bring-out full dirname to stdout, followed by a null character.
-# + `\0` := is identical with null-character in Bash.
+# Exp2: Using `find -print0` option to extricate/bring-out full dirname to stdout, followed by a null character.
+# + `\0` := Identical with null-character/terminate-character in Bash (and Bash had been written by C).
 find . -maxdepth 1 -type d -print0 | while read -d $'\0' dir; do
-  du -h --max-depth=1 $dir;
+  du -h --max-depth=1 $dir
 done
 
-# Exp3: Meesing around with `read` command.
-while IFS=: read userName passWord userID groupID geCos homeDir userShell
-do
+# Exp3: Messing around with `read` command.
+while IFS=: read userName passWord userID groupID geCos homeDir userShell; do
   echo "$userName -> $homeDir"
-done < /etc/passwd
+done </etc/passwd
 
 # Exp4: This example was not offically be compeleted (or incomplete/flawed/unfinished).
-IFS=$'\n' read -d '' -r arr <<< `find . -maxdepth 1 -type d -print0`
+IFS=$'\n' read -d '' -r arr <<<$(find . -maxdepth 1 -type d -print0)
 for d in ${arr[@]}; do echo $d; done
