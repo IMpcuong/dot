916fb3935ea63f1623768cd3104775fbfbc0d5d8
IMpossible <58588543+IMpcuong@users.noreply.github.com>
2023-07-30 20:30:40 -0700

    script: Manage to measure the most captured disk space file in this current directory
    Indexing our examples list's order by their increment of positioning.

--- bash-script/utilities/xargs_examples.sh
+++ bash-script/utilities/xargs_examples.sh
@@ -14,8 +14,11 @@ ps aux |
   tail -n+1 |
   xargs -I{} sh -c 'journalctl _PID={}'
 
-# Exp2: Download a desired MySQL community server RPM package from the MySQL Yum Repository official repositories.
+# Exp3: Download a desired MySQL community server RPM package from the MySQL Yum Repository official repositories.
 curl -s "https://dev.mysql.com/downloads/repo/yum/" |
   grep -oE "mysql80.*-el7-.*rpm" |
   head -n1 |
   xargs -I{} sh -c 'curl -sSLO "https://dev.mysql.com/get/{}"'
+
+# Exp4: Manage to measure the most captured disk space file in this current directory.
+find . -type f ! -newermt "6 months ago" -a -size +0M | xargs -I{} bash -c "du -sh {}"
