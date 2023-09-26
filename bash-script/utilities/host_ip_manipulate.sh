491dcc4db3b85e1ec9d7f3124c5770504d28f3eb
IMpossible <58588543+IMpcuong@users.noreply.github.com>
2023-09-25 21:21:27 -0700

    script: Harvest IP-Private using only plain/severe `ip` command

--- bash-script/utilities/host_ip_manipulate.sh
+++ bash-script/utilities/host_ip_manipulate.sh
@@ -30,7 +30,8 @@ route -n | awk '/UG[ \t]/ { print $2 }' # NOTE: Retrieve host IP address.
 # Retrieve the subnet-mask of the local machine in format `/24 := 32 - 24 == 8 ~ host-ip`:
 # `ip -o/-oneline` := Output each record in the single line, replacing line feeds with the '\' character.
 # `ip -f/-family` := Specifies the protocol family to use; protool family identifier := [inet, inet6, bridge, ipx, dnet, link].
-ip -o -f inet addr show | awk '/scope global/ { print $4 }'
+ip -o -f inet addr show | awk '/scope global/ { print $4 }' | cut -d'/' -f1
+ip -4 a show | grep -oE "inet (10|172)(\.[[:digit:]]{2,3}){3}" | cut -d' ' -f2
 ip -4 a show eth0
 ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p'
 
