a52eeb0b1da50125fc3fb43bee73d3d78fa5cf6a
IMpossible <58588543+IMpcuong@users.noreply.github.com>
2023-10-05 02:33:57 -0700

    chore: Populate camel-case convention on PS1 script

--- pwsh-script/ps1/os_version.ps1
+++ pwsh-script/ps1/os_version.ps1
@@ -1,3 +1,3 @@
 [System.Environment]::OSVersion.Version
 [Environment]::OSVersion.Version -ge (New-Object 'Version' 6,1)
-(Get-WmiObject -class Win32_OperatingSystem).Caption
+(Get-WmiObject -Class Win32_OperatingSystem).Caption
