7134c10ac3972fd1862d85e39403ffb3949cb0ba
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2025-10-15 15:00:22 +0700

    script: Search files' content which are containing a regex-pattern string (Git)

--- bash-script/utilities/commit_contains_pattern.sh
+++ bash-script/utilities/commit_contains_pattern.sh
@@ -9,6 +9,6 @@ declare -a commits=(`git log --oneline | awk '{ print $1 }'`)
 for ((i=0; i<=$depth; i++)); do
   declare -x commit=${commits[$i]}
   declare -x matched=$(git show --no-notes --pretty="%h" "$commit" | LC_ALL=C grep -iE "$pattern")
-  [[ "$matched" == "" ]] || { echo ">>>>> Commit:" $commit; git diff-tree -r --no-commit-id --name-only $commit; }
+  [[ "$matched" == "" ]] || { echo ">>>>> [$i] Commit:" $commit; git diff-tree -r --no-commit-id --name-only $commit; }
   # [[ "$matched" == "" ]] || { echo ">>>>> Commit:" $commit; git show --name-only $commit --oneline; }
 done
